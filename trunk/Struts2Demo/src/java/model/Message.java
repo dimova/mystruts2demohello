1bfd8f6b87bf3379da6278f73c3f12d2379376d6
dimova@gmail.com <dimova@gmail.com@c0d80ff6-61dc-8296-4a72-c4a29fec25d8>
2012-10-21 11:30:46 +0000

    Importing my Struts2 Demo Project.

---
+++ trunk/Struts2Demo/src/java/model/Message.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Desi
+ */
+public class Message {
+    private String message = "Hello World!";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
